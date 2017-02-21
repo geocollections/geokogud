03779d09ee2a4d7a255ecfc9084c7b15935ec859
Stanislav <nazmutdinov@gmail.com>
2017-02-21 20:17:01 +0200

    Fixed the project to work with gradle 3.4

--- src/main/java/ee/ttu/geodeesia/App.java
+++ src/main/java/ee/ttu/geodeesia/App.java
@@ -1,42 +1,39 @@
 package ee.ttu.geodeesia;
 
-import java.util.*;
-import java.util.concurrent.Executors;
-
 import com.google.common.eventbus.AsyncEventBus;
 import com.google.common.eventbus.EventBus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+import java.util.concurrent.Executors;
 
 @Configuration
 @ComponentScan(basePackages = {
         "ee.ttu.geodeesia"
 })
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class App extends SpringBootServletInitializer {
     private static final Logger logger = LoggerFactory.getLogger(App.class);
 
     public static void main(String[] args) throws Exception {
         SpringApplication app = new SpringApplication(App.class);
-        app.setShowBanner(false);
+        app.setBannerMode(Banner.Mode.OFF);
         app.addListeners(new ApplicationListener<ContextRefreshedEvent>() {
             @Override
             public void onApplicationEvent(ContextRefreshedEvent event) {
@@ -45,7 +42,6 @@ public class App extends SpringBootServletInitializer {
             }
         });
         app.run(args);
-
     }
 
     @Override
