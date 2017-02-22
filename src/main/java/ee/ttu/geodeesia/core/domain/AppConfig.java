a51fa5e1155dc280addbd7932f590f7714131ebb
Marjana Voronina <marjana.voronina@gmail.com>
2017-02-22 19:05:20 +0200

    Issue #1: navigation, index page, footer, news section

--- src/main/java/ee/ttu/geodeesia/core/domain/AppConfig.java
+++ src/main/java/ee/ttu/geodeesia/core/domain/AppConfig.java
@@ -15,4 +15,18 @@ public class AppConfig {
     @Value("${fossils.url}")
     public String fossilsUrl;
 
+    @Value("${geo-api.url}")
+    public String apiUrl;
+    
+    @Value("${tug.url}")
+    public String tugUrl;
+    
+    @Value("${elm.url}")
+    public String elmUrl;
+    
+    @Value("${geocase.url}")
+    public String geocaseUrl;
+    
+    @Value("${github.url}")
+    public String githubUrl;
 }
