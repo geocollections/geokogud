f2422f64e428f48bc943c2315a897e0be88b21fc
Liis Harjo <liis@snowhound.eu>
2017-02-24 13:58:15 +0200

    issue #8

--- src/main/webapp/app/search/drill_cores.js
+++ src/main/webapp/app/search/drill_cores.js
@@ -1,3 +1,17 @@
 angular.module('search').controller('SearchDrillCoresController', function($scope, $uibModal){
 
+    $scope.departments = [
+        {code:"GIT",label:"GIT"},
+        {code:"TUG",label:"TUG"},
+        {code:"ELM",label:"ELM"},
+        {code:"TUGO",label:"TUGO"},
+        {code:"MUMU",label:"MUMU"},
+        {code:"EGK",label:"EGK"}];
+
+    $scope.sortbyOptions = [
+        {  name: 'ID', value: 'id' },
+        {  name: 'Locality', value: 'Locality' },
+        {  name: 'Box count', value: 'Box count' }
+    ];
+
 });
