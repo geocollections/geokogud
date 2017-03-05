c258227ee58aced224a8dc10bec6ae389893ba28
Stanislav <nazmutdinov@gmail.com>
2017-03-05 19:27:52 +0200

    [#52] Search parameters are shown in url - analyses search

--- src/main/webapp/app/modules/analyses/search/analyses.js
+++ src/main/webapp/app/modules/analyses/search/analyses.js
@@ -1,48 +1,82 @@
-angular.module('search').controller('SearchAnalysesController', function($scope, SearchService){
+angular.module('search.analyses', []).config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider.state('search.analyses', {
+        url: "/analyses",
+        templateUrl: "app/modules/analyses/search/analyses.html",
+        controller: "SearchAnalysesController",
+        reloadOnSearch: false
+    });
+});
+angular.module('search').controller('SearchAnalysesController', function ($scope, $stateParams, $location, $state, SearchService) {
     $scope.isIdentifierFieldsCollapsed = false;
     $scope.isLocationFieldsCollapsed = false;
     $scope.isAnalysesFieldsCollapsed = false;
     $scope.isInstitutionsCollapsed = false;
     $scope.sortbyOptions = [
-        {  name: 'ID', value: 'id' },
-        {  name: 'Number', value: 'number' },
-        {  name: 'Locality', value: 'locality' },
-        {  name: 'Depth (m)', value: 'depth' },
-        {  name: 'Stratigraphy', value: 'stratigraphy' },
-        {  name: 'Collector', value: 'collector' },
+        {name: 'ID', value: 'id'},
+        {name: 'Number', value: 'number'},
+        {name: 'Locality', value: 'locality'},
+        {name: 'Depth (m)', value: 'depth'},
+        {name: 'Stratigraphy', value: 'stratigraphy'},
+        {name: 'Collector', value: 'collector'},
     ];
     $scope.departments = [
-        {code:"GIT",label:"GIT"},
-        {code:"TUG",label:"TUG"},
-        {code:"ELM",label:"ELM"},
-        {code:"TUGO",label:"TUGO"},
-        {code:"MUMU",label:"MUMU"},
-        {code:"EGK",label:"EGK"}];
+        {code: "GIT", label: "GIT"},
+        {code: "TUG", label: "TUG"},
+        {code: "ELM", label: "ELM"},
+        {code: "TUGO", label: "TUGO"},
+        {code: "MUMU", label: "MUMU"},
+        {code: "EGK", label: "EGK"}];
 
-    $scope.searchDefault = function(search) {
-        if(!search) search = "analysis";
-        SearchService.getSearch(search).then(function(search) {
+    $scope.searchDefault = function (search) {
+        if (!search) search = "analysis";
+        //
+        SearchService.getSearch(search).then(function (search) {
             $scope.sampleSearch = search;
-            $scope.sampleSearch.sortAsc = true;
-            $scope.sampleSearch.orderBy = "id";
             $scope.search();
         });
     };
 
     $scope.searchDefault();
 
-    $scope.search = function() {
-        SearchService.listSearch($scope.sampleSearch).then(function(result) {
+    $scope.search = function () {
+        var s = $scope.analysesSearch;
+
+        //TODO: what to do with INSTITUTIONS? (list)
+        $location.search({
+            idValue: returnValue(s.id),
+            idType: returnType(s.id),
+            numberValue: returnValue(s.number),
+            numberType: returnType(s.number),
+            bedIndexValue: returnValue(s.bedIndex),
+            bedIndexType: returnType(s.bedIndex),
+            localityValue: returnValue(s.locality),
+            localityType: returnType(s.locality),
+            adminunitValue: returnValue(s.adminunit),
+            adminunitType: returnType(s.adminunit),
+            depthValue: returnValue(s.depth),
+            depthType: returnType(s.depth),
+            stratigraphyValue: returnValue(s.stratigraphy),
+            stratigraphyType: returnType(s.stratigraphy),
+            analysisTypeValue: returnValue(s.analysisType),
+            analysisTypeType: returnType(s.analysisType),
+            componentAnalysedValue: returnValue(s.componentAnalysed),
+            componentAnalysedType: returnType(s.componentAnalysed),
+            contentValue: returnValue(s.content),
+            contentType: returnType(s.content)
+        });
+        SearchService.listSearch($scope.sampleSearch).then(function (result) {
             $scope.totalItems = result.count;
             $scope.pageSize = 100;
             $scope.response = result;
+            console.log($location.search().bedIndexValue);
         });
     };
 
-    $scope.order = function(predicate) {
-        $scope.sampleSearch.sortAsc = ($scope.sampleSearch.orderBy === predicate) ? !$scope.sampleSearch.sortAsc : true;
-        $scope.sampleSearch.orderBy = predicate;
-        $scope.search();
-    };
+    function returnValue(criterion) {
+        return criterion.value != null && criterion.value != '' ? criterion.value : null;
+    }
 
+    function returnType(criterion) {
+        return criterion.value != null && criterion.value != '' ? criterion.type : null;
+    }
 });
