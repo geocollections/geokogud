47219c465c9a2e811360930c764b3e58d0e7d908
Olesja Senkiv <olesja.senkiv@ttu.ee>
2017-03-13 20:30:19 +0200

    reference search

--- src/main/java/ee/ttu/geodeesia/interop/api/Response/ApiResponse.java
+++ src/main/java/ee/ttu/geodeesia/interop/api/Response/ApiResponse.java
@@ -2,17 +2,7 @@ package ee.ttu.geodeesia.interop.api.Response;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import ee.ttu.geodeesia.interop.api.analyses.pojo.AnalysesEntity;
-import ee.ttu.geodeesia.interop.api.drillCores.pojo.DrillCoresEntity;
-import ee.ttu.geodeesia.interop.api.localities.pojo.LocalityEntity;
-import ee.ttu.geodeesia.interop.api.photoArchive.pojo.PhotoArchiveEntity;
-import ee.ttu.geodeesia.interop.api.reference.pojo.ReferenceEntity;
-import ee.ttu.geodeesia.interop.api.samples.pojo.SampleEntity;
-import ee.ttu.geodeesia.interop.api.specimen.pojo.SpecimenEntity;
-import ee.ttu.geodeesia.interop.api.stratigraphies.pojo.StratigraphyEnitity;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
---
+++ src/main/java/ee/ttu/geodeesia/interop/api/reference/service/impl/ReferenceApiServiceImpl.java
@@ -0,0 +1,41 @@
+package ee.ttu.geodeesia.interop.api.reference.service.impl;
+
+import ee.ttu.geodeesia.interop.api.Response.Response;
+import ee.ttu.geodeesia.interop.api.builder.FluentGeoApiBuilder;
+import ee.ttu.geodeesia.interop.api.reference.pojo.ReferenceApiResponse;
+import ee.ttu.geodeesia.interop.api.reference.pojo.ReferenceSearchCriteria;
+import ee.ttu.geodeesia.interop.api.reference.service.ReferenceApiService;
+import ee.ttu.geodeesia.interop.api.service.ApiService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReferenceApiServiceImpl implements ReferenceApiService {
+    @Autowired
+    private ApiService apiService;
+
+    @Override
+    public Response findReference(ReferenceSearchCriteria searchCriteria) {
+        String requestParams = FluentGeoApiBuilder.aRequest()
+                .queryId(searchCriteria.getId()).andReturn()
+                .queryAuthor(searchCriteria.getAuthor()).andReturn()
+                .queryTitle(searchCriteria.getTitle()).andReturn()
+                .queryYear(searchCriteria.getYear()).andReturn()
+                .queryDoi(searchCriteria.getDoi()).andReturn()
+                .build();
+        return apiService.findEntity("reference", searchCriteria.getPage(), searchCriteria.getSortField(), requestParams, ReferenceApiResponse.class);
+    }
+
+    @Override
+    public Response findDoi(ReferenceSearchCriteria searchCriteria) {
+        String requestParams = FluentGeoApiBuilder.aRequest()
+                .queryId(searchCriteria.getId()).andReturn()
+                .queryAuthor(searchCriteria.getAuthor()).andReturn()
+                .queryTitle(searchCriteria.getTitle()).andReturn()
+                .queryYear(searchCriteria.getYear()).andReturn()
+                .queryDoi(searchCriteria.getDoi()).andReturn()
+                .fieldIsNotNull("doi")
+                .build();
+        return apiService.findEntity("reference", searchCriteria.getPage(), searchCriteria.getSortField(), requestParams, ReferenceApiResponse.class);
+    }
+}
