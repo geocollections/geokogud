2cef69d5f006d1e4c44996526dcb3c64da4234c1
Olesja Senkiv <olesja.senkiv@ttu.ee>
2017-04-04 00:17:04 +0300

    sample detail form dynamic load field name and data

--- src/main/webapp/app/js/detail.controller.js
+++ src/main/webapp/app/js/detail.controller.js
@@ -2,7 +2,7 @@ var constructor = function ($scope, $stateParams, applicationService) {
     var vm = this;
 
     vm.service = applicationService;
-
+    vm.fields = [];
     searchEntity();
 
     function searchEntity () {
@@ -12,7 +12,20 @@ var constructor = function ($scope, $stateParams, applicationService) {
     function onEntityData(response) {
         console.log(response.data.result[0]);
         vm.sample = response.data.result[0];
+        dynamicalyCreateField(vm.sample);
     }
+
+    function dynamicalyCreateField(obj) {
+        Object.keys(obj).forEach(function(key,index) {
+            console.log(key);
+            console.log(vm.sample[key]);
+            if(vm.sample[key] != null && vm.sample[key] != '')
+                vm.fields.push({"name" : key, "value" : vm.sample[key]});
+            // key: the name of the object key
+            // index: the ordinal position of the key within the object
+        });
+    }
+
 
 };
 
