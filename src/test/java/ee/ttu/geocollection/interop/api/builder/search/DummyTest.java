70e0e76302618759a64952a9d72989a08c13280c
Stanislav <nazmutdinov@gmail.com>
2017-04-19 20:23:25 +0300

    made gradle test task run all test even if some of them fail

---
+++ src/test/java/ee/ttu/geocollection/interop/api/builder/search/DummyTest.java
@@ -0,0 +1,10 @@
+package ee.ttu.geocollection.interop.api.builder.search;
+
+import org.junit.Test;
+
+public class DummyTest {
+    @Test
+    public void badBadCodeUnderTheTest() {
+        throw new NullPointerException();
+    }
+}
