6de28df5d3d912ce4fb2d006bcca46aef99d4af1
Stanislav <nazmutdinov@gmail.com>
2017-04-23 17:41:10 +0300

    [#104] fixed deploy

--- src/main/java/ee/ttu/geocollection/indexing/GlobalSearchApiService.java
+++ src/main/java/ee/ttu/geocollection/indexing/GlobalSearchApiService.java
@@ -1,7 +1,12 @@
 package ee.ttu.geocollection.indexing;
 
+import org.springframework.context.annotation.Profile;
+
 import java.util.Map;
 
+import static ee.ttu.geocollection.EnvironmentConstants.PRODUCTION;
+
+@Profile(PRODUCTION)
 public interface GlobalSearchApiService {
     Map<String, Object> searchGlobally(String query);
 }
