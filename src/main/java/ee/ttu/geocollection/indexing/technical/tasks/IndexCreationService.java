03c2e8b784a91e7e3723926d119d185942b38e86
Stanislav <nazmutdinov@gmail.com>
2017-04-25 00:20:46 +0300

    [#104] Turned off indexing on all environments

--- src/main/java/ee/ttu/geocollection/indexing/technical/tasks/IndexCreationService.java
+++ src/main/java/ee/ttu/geocollection/indexing/technical/tasks/IndexCreationService.java
@@ -2,6 +2,7 @@ package ee.ttu.geocollection.indexing.technical.tasks;
 
 import ee.ttu.geocollection.indexing.IndexService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -9,14 +10,19 @@ import java.util.List;
 
 @Service
 public class IndexCreationService {
+    @Value("${globalSearch.indexing}")
+    private boolean indexing;
+
     @Autowired
     private List<IndexService> indexServices;
 
     /**
-     * Executed only once only if indices recreation is required
+     * Task is executed every 24H, indexing is performed only when appropriate flag is set to true
      */
-    @Scheduled(initialDelay = 1500, fixedRate = Long.MAX_VALUE)
+    @Scheduled(initialDelay = 1500, fixedRate = 86400000)
     public void createIndices() {
-        indexServices.forEach(IndexService::createUpdateIndex);
+        if(indexing) {
+            indexServices.forEach(IndexService::createUpdateIndex);
+        }
     }
 }
