e03a10be4312627b4979cf748b973dad40a3a4b9
Marjana Voronina <marjana.voronina@gmail.com>
2017-05-08 13:12:38 +0300

    UI tests added

---
+++ src/test/java/ee/ttu/geocollection/uiTests/UITests.java
@@ -0,0 +1,387 @@
+package ee.ttu.geocollection.uiTests;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+
+public class UITests {
+    private WebDriver driver;
+    private String baseUrl;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @Before
+    public void setUp() throws Exception {
+        // TODO pass as params
+        System.setProperty("phantomjs.binary.path", "phantomjs/phantomjs-2.1.1-linux-x86_64/bin/phantomjs");
+        driver = new PhantomJSDriver();
+        driver.manage().window().setSize(new Dimension(1920, 1080));
+        baseUrl = "http://geocollections.arendus.geokogud.info";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testAboutCollectionsContentLoaded() throws Exception {
+        driver.get(baseUrl + "/");
+        for (int second = 0;; second++) {
+            if (second >= 60)
+                fail("timeout");
+            try {
+                if (isElementPresent(By.id("langEn")))
+                    break;
+            } catch (Exception e) {
+            }
+            Thread.sleep(1000);
+        }
+
+        driver.findElement(By.id("langEn")).click();
+        driver.findElement(By.linkText("USEFUL INFO")).click();
+        driver.findElement(By.linkText("About collections")).click();
+        try {
+            assertTrue(driver.findElement(By.id("geocollections")).getText()
+                    .matches("^[\\s\\S]*About geological[\\s\\S]*$"));
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+    }
+
+    @Test
+    public void testAboutDatabaseContentLoaded() throws Exception {
+        driver.get(baseUrl + "/");
+        for (int second = 0;; second++) {
+            if (second >= 60)
+                fail("timeout");
+            try {
+                if (isElementPresent(By.id("langEn")))
+                    break;
+            } catch (Exception e) {
+            }
+            Thread.sleep(1000);
+        }
+
+        driver.findElement(By.id("langEn")).click();
+        driver.findElement(By.linkText("USEFUL INFO")).click();
+        driver.findElement(By.linkText("Database")).click();
+        try {
+            assertTrue(driver.findElement(By.id("database")).getText()
+                    .matches("(?i:[\\s\\S]*database for geocollections[\\s\\S]*)"));
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+    }
+
+    @Test
+    public void testChangeLanguage() throws Exception {
+        driver.get(baseUrl + "/");
+        for (int second = 0;; second++) {
+            if (second >= 60)
+                fail("timeout");
+            try {
+                if (isElementPresent(By.id("langEt")))
+                    break;
+            } catch (Exception e) {
+            }
+            Thread.sleep(1000);
+        }
+
+        driver.findElement(By.id("langEt")).click();
+        try {
+            assertTrue(driver.findElement(By.className("navbar-brand")).getText().matches("(?i:.*eesti.*)"));
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        driver.findElement(By.id("langEn")).click();
+        try {
+            assertTrue(driver.findElement(By.className("navbar-brand")).getText().matches("(?i:.*estonia.*)"));
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+    }
+
+    @Test
+    public void testChooseSearchType() throws Exception {
+        driver.get(baseUrl + "/");
+        for (int second = 0;; second++) {
+            if (second >= 60)
+                fail("timeout");
+            try {
+                if (isElementPresent(By.id("langEn")))
+                    break;
+            } catch (Exception e) {
+            }
+            Thread.sleep(1000);
+        }
+
+        driver.findElement(By.id("langEn")).click();
+        driver.findElement(By.linkText("SEARCH")).click();
+        for (int second = 0;; second++) {
+            if (second >= 60)
+                fail("timeout");
+            try {
+                if (isElementPresent(By.cssSelector(".nav-tabs")))
+                    break;
+            } catch (Exception e) {
+            }
+            Thread.sleep(1000);
+        }
+
+        ArrayList<WebElement> tabElements = (ArrayList<WebElement>) driver
+                .findElements(By.cssSelector(".nav-tabs li a"));
+
+        ArrayList<String> tabNames = new ArrayList<String>();
+
+        for (WebElement webElement : tabElements) {
+            tabNames.add(webElement.getText());
+        }
+
+        for (String curTabName : tabNames) {
+            driver.findElement(By.linkText(curTabName)).click();
+
+            for (int second = 0;; second++) {
+                if (second >= 60)
+                    fail("timeout");
+                try {
+                    if (driver.findElement(By.className("search-heading")).getText()
+                            .matches("[\\s\\S]*" + curTabName + "[\\s\\S]*")) {
+                        break;
+                    }
+                } catch (Exception e) {
+                }
+                Thread.sleep(1000);
+            }
+        }
+    }
+
+    @Test
+    public void testFooterLoadedOnExtendedSearchPage() throws Exception {
+        driver.get(baseUrl + "/");
+        for (int second = 0;; second++) {
+            if (second >= 60)
+                fail("timeout");
+            try {
+                if (isElementPresent(By.id("langEn")))
+                    break;
+            } catch (Exception e) {
+            }
+            Thread.sleep(1000);
+        }
+
+        driver.findElement(By.id("langEn")).click();
+        driver.findElement(By.linkText("SEARCH")).click();
+        for (int second = 0;; second++) {
+            if (second >= 60)
+                fail("timeout");
+            try {
+                if (isElementPresent(By.cssSelector("footer")))
+                    break;
+            } catch (Exception e) {
+            }
+            Thread.sleep(1000);
+        }
+
+    }
+
+    @Test
+    public void testFooterLoadedOnMainPage() throws Exception {
+        driver.get(baseUrl + "/sample");
+        for (int second = 0;; second++) {
+            if (second >= 60)
+                fail("timeout");
+            try {
+                if (isElementPresent(By.cssSelector("footer")))
+                    break;
+            } catch (Exception e) {
+            }
+            Thread.sleep(1000);
+        }
+    }
+
+    @Test
+    public void testGeneralInfoLoaded() throws Exception {
+        driver.get(baseUrl + "/");
+        for (int second = 0;; second++) {
+            if (second >= 60)
+                fail("timeout");
+            try {
+                if (isElementPresent(By.id("langEn")))
+                    break;
+            } catch (Exception e) {
+            }
+            Thread.sleep(1000);
+        }
+
+        driver.findElement(By.id("langEn")).click();
+        for (int second = 0;; second++) {
+            if (second >= 60)
+                fail("timeout");
+            try {
+                if (isElementPresent(By.id("about")))
+                    break;
+            } catch (Exception e) {
+            }
+            Thread.sleep(1000);
+        }
+
+        try {
+            assertTrue(
+                    driver.findElement(By.id("about")).getText().matches("^[\\s\\S]*Collections in geology[\\s\\S]*$"));
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        try {
+            assertTrue(driver.findElement(By.id("about")).getText()
+                    .matches("^[\\s\\S]*Geocollections in Estonia[\\s\\S]*$"));
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        try {
+            assertTrue(driver.findElement(By.id("about")).getText().matches("^[\\s\\S]*Common database[\\s\\S]*$"));
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+    }
+
+    @Test
+    public void testGitContentLoaded() throws Exception {
+        driver.get(baseUrl + "/");
+        for (int second = 0;; second++) {
+            if (second >= 60)
+                fail("timeout");
+            try {
+                if (isElementPresent(By.linkText("ENG")))
+                    break;
+            } catch (Exception e) {
+            }
+            Thread.sleep(1000);
+        }
+
+        driver.findElement(By.id("langEn")).click();
+        driver.findElement(By.linkText("USEFUL INFO")).click();
+        driver.findElement(By.linkText("GIT")).click();
+        try {
+            assertTrue(driver.findElement(By.id("git")).getText()
+                    .matches("^[\\s\\S]*GIT: Geological collections[\\s\\S]*$"));
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+    }
+
+    @Test
+    public void testModalWindowIsOpenOnSearchPage() throws Exception {
+        driver.get(baseUrl + "/sample");
+        for (int second = 0;; second++) {
+            if (second >= 60)
+                fail("timeout");
+            try {
+                if (isElementPresent(By.id("langEn")))
+                    break;
+            } catch (Exception e) {
+            }
+            Thread.sleep(1000);
+        }
+
+        driver.findElement(By.id("langEn")).click();
+        for (int second = 0;; second++) {
+            if (second >= 60)
+                fail("timeout");
+            try {
+                if (isElementPresent(By.cssSelector("tr.ng-scope:nth-child(1) > td:nth-child(3) > a:nth-child(1)")))
+                    break;
+            } catch (Exception e) {
+            }
+            Thread.sleep(1000);
+        }
+        driver.findElement(By.cssSelector("tr.ng-scope:nth-child(1) > td:nth-child(3) > a:nth-child(1)")).click();
+        ArrayList<String> browserTabs = new ArrayList<String>(driver.getWindowHandles());
+        // switch to a new tab
+        driver.switchTo().window(browserTabs.get(1));
+        try {
+            assertTrue(driver.getCurrentUrl().matches("^[\\s\\S]*/sample/[\\d]+$"));
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        driver.close();
+        driver.switchTo().window(browserTabs.get(0));
+    }
+
+    @Test
+    public void testShowMap() throws Exception {
+        driver.get(baseUrl + "/");
+        for (int second = 0;; second++) {
+            if (second >= 60)
+                fail("timeout");
+            try {
+                if (isElementPresent(By.id("langEn")))
+                    break;
+            } catch (Exception e) {
+            }
+            Thread.sleep(1000);
+        }
+
+        driver.findElement(By.id("langEn")).click();
+        driver.findElement(By.linkText("MAP")).click();
+        for (int second = 0;; second++) {
+            if (second >= 60)
+                fail("timeout");
+            try {
+                if (isElementPresent(By.id("map")))
+                    break;
+            } catch (Exception e) {
+            }
+            Thread.sleep(1000);
+        }
+    }
+
+    @Test
+    public void testUsingCollectionsContentLoaded() throws Exception {
+        driver.get(baseUrl + "/");
+        for (int second = 0;; second++) {
+            if (second >= 60)
+                fail("timeout");
+            try {
+                if (isElementPresent(By.id("langEn")))
+                    break;
+            } catch (Exception e) {
+            }
+            Thread.sleep(1000);
+        }
+
+        driver.findElement(By.id("langEn")).click();
+        driver.findElement(By.linkText("USEFUL INFO")).click();
+        driver.findElement(By.linkText("Using collections")).click();
+        try {
+            assertTrue(driver.findElement(By.id("usingCollections")).getText().matches("^[\\s\\S]*Using[\\s\\S]*$"));
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+}
