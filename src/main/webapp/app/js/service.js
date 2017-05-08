84f705b64b66c2b21f0b9d0eca801b7031918654
PrintScr <nazmutdinov@gmail.com>
2017-05-08 16:58:05 +0300

    [#70] Added client side

--- src/main/webapp/app/js/service.js
+++ src/main/webapp/app/js/service.js
@@ -1,6 +1,6 @@
 var module = angular.module("geoApp");
 
-var constructor = function (utils,configuration, $window) {
+var constructor = function (utils,configuration, $window, $location) {
 
     var service = {};
 
@@ -11,6 +11,7 @@ var constructor = function (utils,configuration, $window) {
     service.showGoogleMap = showGoogleMap;
     service.showEstonianLandBoardMap = showEstonianLandBoardMap;
     service.getTranslationRoot = getTranslationRoot;
+    service.location = $location;
 
     service.toggle = function (el,array) {
         utils.toggleInArray(el,array)
@@ -79,8 +80,25 @@ var constructor = function (utils,configuration, $window) {
     }
     function composeImageExternalPath (imageData) {
         //            http://geokogud.info/di.php?f=/var/www/git/image/OH/OH07-1/OH07-1-4.jpg
-        return "http://geokogud.info/di.php?f=/var/www/"+imageData.database__acronym.toLowerCase()+"/image/"
-            + imageData.imageset__imageset_series + "/"+imageData.imageset__imageset_number+"/"+imageData.filename;
+        var applicationUrl = buildApplicationUrl();
+        var imageUrl = applicationUrl
+            + "img/"
+            +imageData.database__acronym.toLowerCase() +"/"
+            +imageData.imageset__imageset_series +"/"
+            +imageData.imageset__imageset_number+"/"
+            +imageData.filename;
+        return imageUrl;
+        // service.location.protocol() + "://" + service.location.host() + ":" + service.location.port()
+        // return "http://geokogud.info/di.php?f=/var/www/"+imageData.database__acronym.toLowerCase()+"/image/"
+        //     + imageData.imageset__imageset_series + "/"+imageData.imageset__imageset_number+"/"+imageData.filename;
+    }
+
+    function buildApplicationUrl() {
+        if(service.location.host().indexOf('localhost') != -1){
+            return service.location.protocol() + "://" + service.location.host() + ":" + service.location.port() + "/";
+        }else{
+            return service.location.protocol() + "://" + service.location.host() + "/";
+        }
     }
 
     function getDetailUrl (searchType) {
@@ -161,7 +179,7 @@ var constructor = function (utils,configuration, $window) {
     return service;
 };
 
-constructor.$inject = ['utils','configuration','$window'];
+constructor.$inject = ['utils','configuration','$window', '$location'];
 
 module.service("ApplicationService", constructor);
 
