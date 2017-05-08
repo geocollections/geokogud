8a93a23b0f4ae819b0a43c56ca217f11ca9a043b
Stanislav <nazmutdinov@gmail.com>
2017-05-08 14:29:33 +0300

    [#70] Fixing psychdelic image rendeing

--- src/main/java/ee/ttu/geocollection/endpoint/web/ImageController.java
+++ src/main/java/ee/ttu/geocollection/endpoint/web/ImageController.java
@@ -67,10 +67,10 @@ public class ImageController {
         BufferedImage resizedImage = new BufferedImage(width, height, type);
         Graphics2D g = resizedImage.createGraphics();
 
-        g.setComposite(AlphaComposite.Src);
-        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-        g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+//        g.setComposite(AlphaComposite.Src);
+//        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+//        g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+//        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
         g.drawImage(originalImage, 0, 0, width, height, null);
         g.dispose();
